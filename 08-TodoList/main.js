d7b0fad2af4b522d508e502eeabaca102bf98603
carlitowey4 <carlosespigarestriano@gmail.com>
2023-11-22 20:46:14 +0100

    updated

--- 08-TodoList/main.js
+++ 08-TodoList/main.js
@@ -1,4 +1,3 @@
-// Coger elementos
 const form = document.querySelector('form');
 const ul = document.querySelector('ul');
 const input = form.querySelector('input');
@@ -8,31 +7,60 @@ const emptyMessage = document.querySelector('.empty p');
 // Contador de tareas
 let taskCounter = 0;
 
+// Cargar tareas desde localStorage al cargar la página
+document.addEventListener('DOMContentLoaded', () => {
+  loadTasksFromLocalStorage();
+  updateTaskCount();
+  checkEmptyMessage();
+});
+
+// Función para cargar tareas desde localStorage
+function loadTasksFromLocalStorage() {
+  const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+  savedTasks.forEach(taskText => {
+    addTaskToDOM(taskText);
+  });
+}
+
+// Función para agregar tarea al DOM
+function addTaskToDOM(taskText) {
+  const li = document.createElement('li');
+  const taskTextElement = document.createElement('p');
+  taskTextElement.textContent = taskText;
+
+  const deleteButton = document.createElement('button');
+  deleteButton.className = 'btn-delete';
+  deleteButton.textContent = 'X';
+  deleteButton.addEventListener('click', () => {
+    ul.removeChild(li);
+    taskCounter--;
+    updateTaskCount();
+    checkEmptyMessage();
+    saveTasksToLocalStorage();
+  });
+
+  li.appendChild(taskTextElement);
+  li.appendChild(deleteButton);
+  ul.appendChild(li);
+
+  taskCounter++;
+  saveTasksToLocalStorage();
+}
+
+// Función para guardar tareas en localStorage
+function saveTasksToLocalStorage() {
+  const tasks = Array.from(ul.querySelectorAll('p')).map(p => p.textContent);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
 // Agregar tarea
 function addTask() {
   const taskText = input.value.trim();
 
   if (taskText) {
-    const li = document.createElement('li');
-    const taskTextElement = document.createElement('p');
-    taskTextElement.textContent = taskText;
-
-    const deleteButton = document.createElement('button');
-    deleteButton.className = 'btn-delete';
-    deleteButton.textContent = 'X';
-    deleteButton.addEventListener('click', () => {
-      ul.removeChild(li);
-      taskCounter--;
-      updateTaskCount();
-      checkEmptyMessage();
-    });
-
-    li.appendChild(taskTextElement);
-    li.appendChild(deleteButton);
-    ul.appendChild(li);
-
+    addTaskToDOM(taskText);
     input.value = '';
-    taskCounter++;
     updateTaskCount();
     checkEmptyMessage();
   }
@@ -53,12 +81,11 @@ function checkEmptyMessage() {
 }
 
 // Evento de envío del formulario
-form.addEventListener('submit', function (mensaje) {
-  mensaje.preventDefault();
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
   addTask();
 });
 
 // Agregar una tarea haciendo clic en el botón
 const addButton = form.querySelector('.btn-add');
 addButton.addEventListener('click', addTask);
-
